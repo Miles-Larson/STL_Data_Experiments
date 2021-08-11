addd1452483a4134ff3abd484e7e116255252cca
Miles Larson <milesflarson@gmail.com>
2021-08-11 14:17:14 -0500

    commit

--- stldata.io/stl_boardbills.py
+++ stldata.io/stl_boardbills.py
@@ -7,7 +7,7 @@ boardbill_df = pd.read_csv(
 
 #Assumptions, Ward is populated and with President info
 #Function edits the dataframe in place
-def clean_bb_data(df:pd.DataFrame):
+def clean_bb_data(df:pd.DataFrame) -> None:
     df['President Proposal'] = boardbill_df['Ward'] == 'President'
     df['Passed'] = boardbill_df['Ordinance'] != 0
     #Remove digits from BB data
@@ -21,14 +21,24 @@ def clean_bb_data(df:pd.DataFrame):
     return None
 
 class data_analysis():
-    def __init__(self,df:pd.DataFrame):
+
+    def __init__(self,df:pd.DataFrame) -> None:
+        """This class is designed to take a dataframe and return answers to analytical questions regarding Board Bill data.
+
+    Expectations include a column for 'Passed', 'Board Bill', 'President Proposal', and 'Ward'
+    """
         self.df = df.copy()
-        self.bills_passed_df = df.get(df['Passed']==True)
         self.number_of_bills = len(df)
-        self.number_of_bills_passed = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[True,'Board Bill']
-        self.number_of_bills_not_passed = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[False,'Board Bill']
+        self.number_of_bills_passed = len(df['Passed']==True)
+        self.number_of_bills_not_passed = len(df['Passed']==False)
 
-        print('hi')
+        self.bills_passed_df = df.get(df['Passed']==True)
+        self._groupby_example = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[False,'Board Bill']
+        print('Data Analysis Complete')
 
+class presentation():
+    def __init__(self) -> None: #todo
+        pass
+    
 clean_bb_data(boardbill_df)
 bb_data_analysis = data_analysis(boardbill_df)
