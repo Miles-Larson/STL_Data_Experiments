772938b84f8e5e2182f7785fb38476fab7eedf76
Miles Larson <milesflarson@gmail.com>
2021-08-11 03:20:27 -0500

    cleanup and creation of data analysis class

--- stldata.io/stl_boardbills.py
+++ stldata.io/stl_boardbills.py
@@ -4,20 +4,31 @@ boardbill_df = pd.read_csv(
     r'C:\Users\\legis\Documents\BoardBill.csv', encoding='iso-8859-1')
 
 
-#Remove digits from BB data
-boardbill_df['Board Bill'] = boardbill_df['Board Bill'].str.replace(
-    pat=r'\D', repl='', regex=True).astype(int)
 
-#Sort data after bb has been turned into int, will sort correctly
-boardbill_df = boardbill_df.sort_values(
-    ['Session', 'Board Bill'], ascending=[True, True])
+#Assumptions, Ward is populated and with President info
+#Function edits the dataframe in place
+def clean_bb_data(df:pd.DataFrame):
+    df['President Proposal'] = boardbill_df['Ward'] == 'President'
+    df['Passed'] = boardbill_df['Ordinance'] != 0
+    #Remove digits from BB data
+    temp = df['Board Bill'].str.replace(pat=r'\D', repl='', regex=True)
+    temp = pd.to_numeric(temp)
+    df['Board Bill'].update(temp)
+    #Remove digits from Ward
+    temp = df['Ward'].str.replace(pat=r'\D', repl='', regex=True).astype(str)
+    
+    df.update(temp)
+    return None
 
-#Analysis 1: Which wards have had the most ordinances come from it?
-#Visualization ideas - ward map over time
-analysis_1_df = boardbill_df[['Session','Ward','Ordinance']].copy()
-analysis_1_df['Passed'] = boardbill_df['Ordinance'].divide(boardbill_df['Ordinance'])
-analysis_1_df['Passed'][analysis_1_df['Passed'] != 1.0] = 0
-analysis_1_df.pop('Ordinance')
-analysis_1_df.groupby(['Session','Ward']).sum()
+class data_analysis():
+    def __init__(self,df:pd.DataFrame):
+        self.df = df.copy()
+        self.bills_passed_df = df.get(df['Passed']==True)
+        self.number_of_bills = len(df)
+        self.number_of_bills_passed = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[True,'Board Bill']
+        self.number_of_bills_not_passed = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[False,'Board Bill']
 
-print(analysis_1_df)
+        print('hi')
+
+clean_bb_data(boardbill_df)
+bb_data_analysis = data_analysis(boardbill_df)
