68d5e8097f326fe2e7734d9815558fd8105f634c
Miles Larson <milesflarson@gmail.com>
2021-08-16 01:26:03 -0500

    fix data analysis class issues

--- stldata.io/stl_boardbills.py
+++ stldata.io/stl_boardbills.py
@@ -32,8 +32,13 @@ class data_analysis():
         self.number_of_bills_passed = len(df['Passed']==True)
         self.number_of_bills_not_passed = len(df['Passed']==False)
 
-        self.bills_passed_df = df.get(df['Passed']==True)
-        self._groupby_example = df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[False,'Board Bill']
+        self.bills_passed_df = self.df.get(df['Passed']==True)
+        self.bills_per_ward_df = self.df[['Session','Ward','Passed','Board Bill']].groupby(['Passed','Ward','Session'],0).count().reset_index().copy()
+        self.bills_per_alderman_df = self.df[['Session','Alderman','Passed','Board Bill']].groupby(['Passed','Alderman','Session'],0).count().reset_index().copy()
+        self._groupby_example = self.df[['Passed', 'Board Bill']].groupby(['Passed']).count().copy().at[False,'Board Bill']
+        self._2015_bb_passed = self.bills_per_ward_df.query('Passed == True & Session == "2015-2016"').get(['Ward','Board Bill']).copy()
+        #self._2015_bb_passed_chart = self._2015_bb_passed.plot(x='Ward', y='Board Bill',kind='Bar')
+        
         print('Data Analysis Complete')
 
 class presentation():
